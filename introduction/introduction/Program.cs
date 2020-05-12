d8ef9822b5f9f52469eb468c1261f40b405b0ff3
tanviredu <ornobtanvir.git@gmail.com>
2020-05-12 23:27:31 +0600

    added

--- introduction/introduction/Program.cs
+++ introduction/introduction/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 
 namespace introduction
 {
@@ -14,26 +15,18 @@ namespace introduction
             // in  folder with ot without usin linq
             string path = @"C:\windows";
             ShowLargeFileWithoutLinq(path);
+            Console.WriteLine("**************************");
+            ShowLargeFileWithLinq(path);
         }
 
+        
         private static void ShowLargeFileWithoutLinq(string path)
         {
 
             // we get the file info from a file.io
             DirectoryInfo directory = new DirectoryInfo(path);
             FileInfo[] files = directory.GetFiles();
-            // now we need to sort the files
-            // but before sort the array
-            // you need to pass a icomparable
-            // object that is used to compare the file 
-            // to sort
-            // and it will sort in the assending order
-
-
             Array.Sort(files,new FileInfoComparer());
-
-            // now we want to print the first 5 file
-            // of the system
             int i = 1;
             foreach (var file in files)
             {
@@ -50,23 +43,29 @@ namespace introduction
             } // we get the name and the size from the file
 
         }
+        private static void ShowLargeFileWithLinq(string path)
+        {
+            // same job with linq
+            // make a query
+            var query = from file in new DirectoryInfo(path).GetFiles() orderby file.Length descending select file;
+
+            // now we get all the file with this query after he execution
+
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"Name :{file.Name,-20} size:{file.Length,10:N0} ");
+            }
+        }
+
     }
 
     public class FileInfoComparer : IComparer<FileInfo>
     {
-        // we are comparing the FileInfo object
-        // based on the length of the file
-        // you need to pass the object of the file
-        // to the Array.sort() method
-        // this will help this array sort()
-        // method to compare the file
+
 
         // this 
         public int Compare(FileInfo file1, FileInfo file2)
         {
-            // this will help to sort the array in assending 
-            // order
-            //return file1.Length.CompareTo(file2.Length);
             return file2.Length.CompareTo(file1.Length);
         }
 
